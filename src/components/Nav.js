9a66b15e5761ab8b4596ea1cbb2a1eb799977769
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-11-23 04:28:25 +1100

    Mobile nav added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import { Link } from 'react-router-dom';
 
@@ -6,8 +6,19 @@ import logo from "../imgs/tabicon.png";
 
 
 function Nav() {
+
+    const openMenu = () => {
+        document.getElementById("mobileLinks").style.width = "200px";
+    }
+
+    const closeMenu = () => {
+        document.getElementById("mobileLinks").style.width = "0px";
+    }
+
+
     return (
-        <nav>
+        <Fragment>
+                    <nav>
             <Link to="/home">
                 <img src={logo} alt="logo" />
             </Link>
@@ -33,6 +44,39 @@ function Nav() {
                 </Link>
             </div>
         </nav>
+
+        <div id="mobileNav">
+            <button id="mobileNavIcon" onClick={openMenu}>
+                ☰
+            </button>
+
+            <div id="mobileLinks">
+                <Link to="/home" onClick={closeMenu}>
+                    Home  
+                </Link>
+                <br />
+                <Link to="/our-story" onClick={closeMenu}>
+                    Our story  
+                </Link>
+                <br />
+                <Link to="/kids-club" onClick={closeMenu}>
+                    Kids Club  
+                </Link>
+                <br />
+                <Link to="/menu" onClick={closeMenu}>
+                    Menu  
+                </Link>
+                <br />
+                <Link to="/catering" onClick={closeMenu}>
+                    Catering  
+                </Link>
+                <br />
+                <Link to="/contact" onClick={closeMenu}>
+                    Contact
+                </Link>
+            </div>
+        </div>
+        </Fragment>
     )
 }
 
