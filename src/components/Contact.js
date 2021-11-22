15a3b9bdb327653482ac0516d416ba547460d0da
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-11-23 03:13:10 +1100

    Added map

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,63 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Nav from "./Nav.js";
+import Map from './Map.js';
 
 function Contact() {
+
+
     return (
-        <div>
-            
-        </div>
+        <Fragment>
+            <Nav />
+            <div id="contact">
+                <main>
+                    <h1>
+                        Contact us
+                    </h1>
+                    <h3>
+                    For questions about any of our products, or help with 
+                    placing your order, don't hesitate to contact us.
+                    <br />
+                    You can either fill in the form or contact us by email
+                   (info@cheekychicken.com.au) or call us at 03 5221 8308
+                    </h3>
+
+                    <form>
+                        <label htmlFor="name">
+                            Name: *
+                        </label>
+                        <input id="name" type="text" required />
+                        <br />
+
+                        <label htmlFor="phone">
+                            Phone: 
+                        </label>
+                        <input id="phone" type="number" />
+                        <br />
+
+                        <label htmlFor="email">
+                            Email *: 
+                        </label>
+                        <input id="email" type="email" required />
+                        <br />
+
+                        <label htmlFor="message">
+                            Message: 
+                        </label>
+                        <br />
+                        <textarea cols="30" rows="5" required>
+
+                        </textarea>
+                        <br />
+
+                        <button type="submit" id="contactBtn">
+                            Submit
+                        </button>
+                    </form>
+                    <Map />
+                    
+                </main>
+            </div>
+        </Fragment>
     )
 }
 
---
+++ src/components/Map.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Map() {
+    return (
+        <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12551.17553073562!2d144.3602076560669!3d-38.1449854814024!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9d7649e4bb73fbf0!2sCheeky%20Chicken!5e0!3m2!1sen!2sau!4v1637597416432!5m2!1sen!2sau" 
+            width="500" 
+            height="400" 
+            allowfullscreen="" 
+            loading="lazy">
+        </iframe>
+    )
+}
+
+export default Map
