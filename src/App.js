6679f1ad387b0468e0942f99609e52af2b9f6f57
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-11-23 05:59:39 +1100

    debugging urls

--- src/App.js
+++ src/App.js
@@ -15,14 +15,14 @@ function App() {
     <Fragment>
       <BrowserRouter>
         <Routes>    
-          <Route path="/cheeky-chicken" element={<Disclaimer />} />
-          <Route path="/" element={<Disclaimer />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/our-story" element={<Story />} />
-          <Route path="/kids-club" element={<KidsClub />} />
-          <Route path="/catering" element={<Catering />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/cheeky-chicken" exact element={<Disclaimer />} />
+          <Route path="/" exact element={<Disclaimer />} />
+          <Route path="/home" exact element={<Home />} />
+          <Route path="/our-story" exact element={<Story />} />
+          <Route path="/kids-club" exact element={<KidsClub />} />
+          <Route path="/catering" exact element={<Catering />} />
+          <Route path="/menu" exact element={<Menu />} />
+          <Route path="/contact" exact element={<Contact />} />
         </Routes>
       </BrowserRouter>
       
